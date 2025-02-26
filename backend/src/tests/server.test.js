95077829078fb10aefcd24bf05a8ff940971da84
alifbintang1 <alifbintange@gmail.com>
2025-02-26 16:48:11 +0700

    [FIX] fix test backend

--- backend/src/tests/server.test.js
+++ backend/src/tests/server.test.js
@@ -1,19 +1,19 @@
+const test = require('ava');
 const request = require('supertest');
 const Express = require('express');
 const Routes = require('../routes.js');
 require('dotenv').config();
 
-describe('Server tests', () => {
-  let app;
-
-  beforeAll(() => {
-    app = Express();
+// Setup sebelum semua test dijalankan
+test.serial.before(async (t) => {
+    const app = Express();
     process.env.API_PREFIX = 'v1';
     app.use(`/${process.env.API_PREFIX}/`, Routes);
-  });
+    t.context.app = app;
+});
 
-  test('should return 200 on a valid API request', async () => {
-    const response = await request(app).get(`/${process.env.API_PREFIX}/ping`);
-    expect(response.status).toBe(200);
-  });
+// Test menggunakan AVA langsung
+test.serial('should return 200 on a valid API request', async (t) => {
+    const response = await request(t.context.app).get(`/${process.env.API_PREFIX}/ping`);
+    t.is(response.status, 200);
 });
