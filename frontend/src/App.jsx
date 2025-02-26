2f4a8bf19eaef32534f4317730a73f75a179bddd
alifbintang1 <alifbintange@gmail.com>
2025-02-27 02:02:30 +0700

    [GREEN] Implement dashboard page for summary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard'; // Pastikan path ini benar
 
 const App = () => {
   return (
