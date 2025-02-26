e4fc6c895ec719b879623d3d546f90064b70292e
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2025-02-27 00:19:17 +0700

    [REFACTOR] Implement page routings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
+
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+
 import './App.css';
+
+import Login from "../src/pages/Login";
 
 const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello, World this is real</h1>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/" element={<div>
+          <img src={reactLogo} alt="React logo" />
+          <img src={viteLogo} alt="Vite logo" />
+          <h1>Hello, World this is real</h1>
+        </div>} />
+      </Routes>
+    </Router>
   );
 };
 
