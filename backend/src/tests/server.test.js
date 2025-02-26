7d5a8942267874885fe1e1c5176c823c4188bf92
alifbintang1 <alifbintange@gmail.com>
2025-02-26 15:58:09 +0700

    [FIX] change AVA to JEST for frontend

--- backend/src/tests/server.test.js
+++ backend/src/tests/server.test.js
@@ -1,18 +1,19 @@
-const test = require('ava');
 const request = require('supertest');
 const Express = require('express');
 const Routes = require('../routes.js');
 require('dotenv').config();
 
-test.serial.before(async (t) => {
-    const app = Express();
+describe('Server tests', () => {
+  let app;
+
+  beforeAll(() => {
+    app = Express();
     process.env.API_PREFIX = 'v1';
     app.use(`/${process.env.API_PREFIX}/`, Routes);
+  });
 
-    t.context.app = app; // Store app instance
-});
-
-test.serial('should return 200 on a valid API request', async (t) => {
-    const response = await request(t.context.app).get(`/${process.env.API_PREFIX}/ping`);
-    t.is(response.status, 200);
+  test('should return 200 on a valid API request', async () => {
+    const response = await request(app).get(`/${process.env.API_PREFIX}/ping`);
+    expect(response.status).toBe(200);
+  });
 });
