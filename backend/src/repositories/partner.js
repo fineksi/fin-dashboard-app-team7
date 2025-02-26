3ad8f6c3fdbed077994777aaadc36c7935e372cc
nabilmuafa <nmuafa@gmail.com>
2025-02-27 00:55:15 +0700

    [GREEN] implement two new functions for auth db transactions

--- backend/src/repositories/partner.js
+++ backend/src/repositories/partner.js
@@ -12,4 +12,14 @@ exports.findByUuid = async (uuid) => {
     return rows[0];
 };
 
+exports.findByEmail = async (email) => {
+    const [rows] = await Db.query(`select * from ${TABLE_NAME} where email = ? and deleted_at is null limit 1`, [email]);
+    return rows[0];
+};
+
+exports.updateLastLogin = async (email) => {
+    const [status] = await Db.query(`UPDATE ${TABLE_NAME} SET last_login_at = NOW() WHERE email = ?`, [email]);
+    return status;
+}
+
 module.exports = exports
