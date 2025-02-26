21e69100968b16be512f111142a94685f4feccdc
nabilmuafa <nmuafa@gmail.com>
2025-02-27 00:56:40 +0700

    [REFACTOR] use prettier on repository src & test

--- backend/src/repositories/partner.js
+++ backend/src/repositories/partner.js
@@ -1,25 +1,37 @@
-const Db = require('./db');
+const Db = require("./db");
 
-const TABLE_NAME = 'partner';
+const TABLE_NAME = "partner";
 
 exports.findById = async (id) => {
-    const [rows] = await Db.query(`select * from ${TABLE_NAME} where id = ? and deleted_at is null limit 1`, [id]);
-    return rows[0];
+  const [rows] = await Db.query(
+    `select * from ${TABLE_NAME} where id = ? and deleted_at is null limit 1`,
+    [id]
+  );
+  return rows[0];
 };
 
 exports.findByUuid = async (uuid) => {
-    const [rows] = await Db.query(`select * from ${TABLE_NAME} where uuid = ? and deleted_at is null limit 1`, [uuid]);
-    return rows[0];
+  const [rows] = await Db.query(
+    `select * from ${TABLE_NAME} where uuid = ? and deleted_at is null limit 1`,
+    [uuid]
+  );
+  return rows[0];
 };
 
 exports.findByEmail = async (email) => {
-    const [rows] = await Db.query(`select * from ${TABLE_NAME} where email = ? and deleted_at is null limit 1`, [email]);
-    return rows[0];
+  const [rows] = await Db.query(
+    `select * from ${TABLE_NAME} where email = ? and deleted_at is null limit 1`,
+    [email]
+  );
+  return rows[0];
 };
 
 exports.updateLastLogin = async (email) => {
-    const [status] = await Db.query(`UPDATE ${TABLE_NAME} SET last_login_at = NOW() WHERE email = ?`, [email]);
-    return status;
-}
+  const [status] = await Db.query(
+    `UPDATE ${TABLE_NAME} SET last_login_at = NOW() WHERE email = ?`,
+    [email]
+  );
+  return status;
+};
 
-module.exports = exports
+module.exports = exports;
--- backend/src/tests/repositories/partner_repository.test.js
+++ backend/src/tests/repositories/partner_repository.test.js
@@ -1,118 +1,172 @@
-const test = require('ava');
-const sinon = require('sinon');
-const proxyquire = require('proxyquire');
+const test = require("ava");
+const sinon = require("sinon");
+const proxyquire = require("proxyquire");
 
 const DbStub = {
-    query: sinon.stub()
+  query: sinon.stub(),
 };
 
-const AuthRepository = proxyquire('../../repositories/partner', {
-  './db': DbStub
+const AuthRepository = proxyquire("../../repositories/partner", {
+  "./db": DbStub,
 });
 
 test.beforeEach(() => {
-    DbStub.query.reset();
+  DbStub.query.reset();
 });
 
-test("findById should return a user if found", async(t) => {
-    const mockUser = { uuid: "5b1ec29e-9c11-416a-b396-a527732f8718", id: 1, email: "sigma@example.com", password: "exampleHashed" };
+test("findById should return a user if found", async (t) => {
+  const mockUser = {
+    uuid: "5b1ec29e-9c11-416a-b396-a527732f8718",
+    id: 1,
+    email: "sigma@example.com",
+    password: "exampleHashed",
+  };
 
-    DbStub.query.resolves([[mockUser], []]);
+  DbStub.query.resolves([[mockUser], []]);
 
-    const user = await AuthRepository.findById(1);
+  const user = await AuthRepository.findById(1);
 
-    t.deepEqual(user, mockUser);
-    t.true(DbStub.query.calledWith("select * from partner where id = ? and deleted_at is null limit 1", [1]));
+  t.deepEqual(user, mockUser);
+  t.true(
+    DbStub.query.calledWith(
+      "select * from partner where id = ? and deleted_at is null limit 1",
+      [1]
+    )
+  );
 });
 
-test("findById should return undefined if user doesn't exist", async(t) => {
-    DbStub.query.resolves([[]]);
+test("findById should return undefined if user doesn't exist", async (t) => {
+  DbStub.query.resolves([[]]);
 
-    const user = await AuthRepository.findById(2);
+  const user = await AuthRepository.findById(2);
 
-    t.deepEqual(user, undefined);
-    t.true(DbStub.query.calledWith("select * from partner where id = ? and deleted_at is null limit 1", [2]));
+  t.deepEqual(user, undefined);
+  t.true(
+    DbStub.query.calledWith(
+      "select * from partner where id = ? and deleted_at is null limit 1",
+      [2]
+    )
+  );
 });
 
-test("findByUuid should return a user if found", async(t) => {
-    const mockUser = { uuid: "5b1ec29e-9c11-416a-b396-a527732f8718", id: 1, email: "sigma@example.com", password: "exampleHashed" };
+test("findByUuid should return a user if found", async (t) => {
+  const mockUser = {
+    uuid: "5b1ec29e-9c11-416a-b396-a527732f8718",
+    id: 1,
+    email: "sigma@example.com",
+    password: "exampleHashed",
+  };
 
-    DbStub.query.resolves([[mockUser], []]);
+  DbStub.query.resolves([[mockUser], []]);
 
-    const user = await AuthRepository.findByUuid("5b1ec29e-9c11-416a-b396-a527732f8718");
+  const user = await AuthRepository.findByUuid(
+    "5b1ec29e-9c11-416a-b396-a527732f8718"
+  );
 
-    t.deepEqual(user, mockUser);
-    t.true(DbStub.query.calledWith("select * from partner where uuid = ? and deleted_at is null limit 1", ["5b1ec29e-9c11-416a-b396-a527732f8718"]));
+  t.deepEqual(user, mockUser);
+  t.true(
+    DbStub.query.calledWith(
+      "select * from partner where uuid = ? and deleted_at is null limit 1",
+      ["5b1ec29e-9c11-416a-b396-a527732f8718"]
+    )
+  );
 });
 
-test("findByUUID should return undefined if user doesn't exist", async(t) => {
-    DbStub.query.resolves([[]]);
+test("findByUUID should return undefined if user doesn't exist", async (t) => {
+  DbStub.query.resolves([[]]);
 
-    const user = await AuthRepository.findByUuid("55555555-ffff-cccc-6666-aaaaaaaaaaaaaa");
+  const user = await AuthRepository.findByUuid(
+    "55555555-ffff-cccc-6666-aaaaaaaaaaaaaa"
+  );
 
-    t.deepEqual(user, undefined);
-    t.true(DbStub.query.calledWith("select * from partner where uuid = ? and deleted_at is null limit 1", ["55555555-ffff-cccc-6666-aaaaaaaaaaaaaa"]));
+  t.deepEqual(user, undefined);
+  t.true(
+    DbStub.query.calledWith(
+      "select * from partner where uuid = ? and deleted_at is null limit 1",
+      ["55555555-ffff-cccc-6666-aaaaaaaaaaaaaa"]
+    )
+  );
 });
 
-test("findByEmail should return a user if found", async(t) => {
-    const mockUser = { uuid: "5b1ec29e-9c11-416a-b396-a527732f8718", id: 1, email: "sigma@example.com", password: "exampleHashed" };
+test("findByEmail should return a user if found", async (t) => {
+  const mockUser = {
+    uuid: "5b1ec29e-9c11-416a-b396-a527732f8718",
+    id: 1,
+    email: "sigma@example.com",
+    password: "exampleHashed",
+  };
 
-    DbStub.query.resolves([[mockUser], []]);
+  DbStub.query.resolves([[mockUser], []]);
 
-    const user = await AuthRepository.findByEmail("sigma@example.com");
+  const user = await AuthRepository.findByEmail("sigma@example.com");
 
-    t.deepEqual(user, mockUser);
-    t.true(DbStub.query.calledWith("select * from partner where email = ? and deleted_at is null limit 1", ["sigma@example.com"]));
+  t.deepEqual(user, mockUser);
+  t.true(
+    DbStub.query.calledWith(
+      "select * from partner where email = ? and deleted_at is null limit 1",
+      ["sigma@example.com"]
+    )
+  );
 });
 
-test("findByEmail should return undefined if user doesn't exist", async(t) => {
-    DbStub.query.resolves([[]]);
+test("findByEmail should return undefined if user doesn't exist", async (t) => {
+  DbStub.query.resolves([[]]);
 
-    const user = await AuthRepository.findByEmail("admin@fineksi.com");
+  const user = await AuthRepository.findByEmail("admin@fineksi.com");
 
-    t.deepEqual(user, undefined);
-    t.true(DbStub.query.calledWith("select * from partner where email = ? and deleted_at is null limit 1", ["admin@fineksi.com"]));
+  t.deepEqual(user, undefined);
+  t.true(
+    DbStub.query.calledWith(
+      "select * from partner where email = ? and deleted_at is null limit 1",
+      ["admin@fineksi.com"]
+    )
+  );
 });
 
-test("findById should return undefined if id provided is null", async(t) => {
-    DbStub.query.resolves([[]]);
-    const user = await AuthRepository.findById(null);
+test("findById should return undefined if id provided is null", async (t) => {
+  DbStub.query.resolves([[]]);
+  const user = await AuthRepository.findById(null);
 
-    t.is(user, undefined);
+  t.is(user, undefined);
 });
 
-test("findByUuid should return undefined if uuid provided is null", async(t) => {
-    DbStub.query.resolves([[]]);
-    const user = await AuthRepository.findByUuid(null);
+test("findByUuid should return undefined if uuid provided is null", async (t) => {
+  DbStub.query.resolves([[]]);
+  const user = await AuthRepository.findByUuid(null);
 
-    t.is(user, undefined);
+  t.is(user, undefined);
 });
 
-test("findByEmail should return undefined if email provided is null", async(t) => {
-    DbStub.query.resolves([[]]);
-    const user = await AuthRepository.findByEmail(null);
+test("findByEmail should return undefined if email provided is null", async (t) => {
+  DbStub.query.resolves([[]]);
+  const user = await AuthRepository.findByEmail(null);
 
-    t.is(user, undefined);
+  t.is(user, undefined);
 });
 
-test("findById should return undefined if id provided is non-numeric", async(t) => {
-    DbStub.query.resolves([[]]);
-    const user = await AuthRepository.findById("Email");
+test("findById should return undefined if id provided is non-numeric", async (t) => {
+  DbStub.query.resolves([[]]);
+  const user = await AuthRepository.findById("Email");
 
-    t.is(user, undefined);
+  t.is(user, undefined);
 });
 
-test("findByEmail should return undefined if email provided is empty string", async(t) => {
-    DbStub.query.resolves([[]]);
-    const user = await AuthRepository.findByEmail("");
+test("findByEmail should return undefined if email provided is empty string", async (t) => {
+  DbStub.query.resolves([[]]);
+  const user = await AuthRepository.findByEmail("");
 
-    t.is(user, undefined);
+  t.is(user, undefined);
 });
 
-test("updateLastLogin should update last_login_at for the logged in user", async(t) => {
-    DbStub.query.resolves([{ affectedRows: 1 }]);
-    
-    await AuthRepository.updateLastLogin("partner@mail.com");
+test("updateLastLogin should update last_login_at for the logged in user", async (t) => {
+  DbStub.query.resolves([{ affectedRows: 1 }]);
 
-    t.true(DbStub.query.calledWith("UPDATE partner SET last_login_at = NOW() WHERE email = ?", ["partner@mail.com"]));
+  await AuthRepository.updateLastLogin("partner@mail.com");
+
+  t.true(
+    DbStub.query.calledWith(
+      "UPDATE partner SET last_login_at = NOW() WHERE email = ?",
+      ["partner@mail.com"]
+    )
+  );
 });
