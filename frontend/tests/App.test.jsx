dd79d6d7167a0e3e535df9a10b64ed73f19eaf76
alifbintang1 <alifbintange@gmail.com>
2025-02-26 21:14:51 +0700

    [REFACTOR] Improving code coverage tests

--- frontend/tests/App.test.jsx
+++ frontend/tests/App.test.jsx
@@ -5,12 +5,16 @@ import App from '../src/App';
 
 describe('App Component', () => {
   test('renders Vite and React logos', () => {
-    render(React.createElement(App));
+    render(<App />);
     const viteLogo = screen.getByAltText('Vite logo');
     const reactLogo = screen.getByAltText('React logo');
 
     expect(viteLogo).toBeInTheDocument();
     expect(reactLogo).toBeInTheDocument();
-    
+  });
+
+  test('renders Hello World text', () => {
+    render(<App />);
+    expect(screen.getByText('Hello, World this is real')).toBeInTheDocument();
   });
 });
