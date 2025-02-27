2946cf058ebf8e65abf5995dc1aaf15b6291da0e
venedictchen <venedictchen@gmail.com>
2025-02-27 14:58:22 +0700

    add prettier

--- backend/src/services/auth_service.js
+++ backend/src/services/auth_service.js
@@ -1,8 +1,7 @@
 const AuthRepository = require("../repositories/partner.js");
-
-exports.login = async (email, password) => {
-    const data = await AuthRepository.findById(email);
-    return data;
-}
+exports.login = async (email) => {
+  const data = await AuthRepository.findById(email);
+  return data;
+};
 
 module.exports = exports;
