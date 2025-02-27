253b7ee446e3f5429ad94604ea6fe84416d7d90d
aldenluthfi <aldenluthfi@gmail.com>
2025-02-27 11:48:13 +0700

    [GREEN] Implement responsiveness to the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import logoFineksi from '../assets/logo fineksi.png';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light fixed-top">
-      <div className="container d-flex justify-content-between">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container">
         <a className="navbar-brand" href="/">
           <img
             src={logoFineksi}
@@ -16,22 +16,46 @@ const Navbar = () => {
           />
         </a>
 
-        <div className="dropdown">
-          <button
-            className="btn btn-link dropdown-toggle text-dark text-decoration-none"
-            type="button"
-            id="userDropdown"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <i className="bi bi-person-circle me-2"></i>
-            User Name
-          </button>
-          <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-            <li><a className="dropdown-item" href="/profile">Profile</a></li>
-            <li><a className="dropdown-item" href="/settings">Settings</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item text-danger" href="/logout">Logout</a></li>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div 
+          className="collapse navbar-collapse" 
+          id="navbarContent"
+          data-testid="navbar-content"
+        >
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item dropdown">
+              <button
+                className="btn btn-link dropdown-toggle text-dark text-decoration-none"
+                type="button"
+                id="userDropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i className="bi bi-person-circle me-2"></i>
+                User Name
+              </button>
+              <ul 
+                className="dropdown-menu dropdown-menu-end" 
+                aria-labelledby="userDropdown"
+                role="menu"
+              >
+                <li><a className="dropdown-item" href="/profile">Profile</a></li>
+                <li><a className="dropdown-item" href="/settings">Settings</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item text-danger" href="/logout">Logout</a></li>
+              </ul>
+            </li>
           </ul>
         </div>
       </div>
